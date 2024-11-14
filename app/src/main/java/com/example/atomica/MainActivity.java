cb056247fe425942a5e237bee88330c38a468332
Aniruddha Biswas Atanu <aniruddhabiswas105@gmail.com>
2024-11-14 01:31:45 -0600

    "icons added for the bottom menu"

--- app/src/main/java/com/example/atomica/MainActivity.java
+++ app/src/main/java/com/example/atomica/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.atomica;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
