313862cba28596611f2cd8f592d799713490eee9
Aniruddha Biswas Atanu <aniruddhabiswas105@gmail.com>
2024-11-14 20:45:38 -0600

    table added

--- app/src/main/java/com/example/atomica/fragments/WebFragment.java
+++ app/src/main/java/com/example/atomica/fragments/WebFragment.java
@@ -2,6 +2,7 @@ package com.example.atomica.fragments;
 
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.atomica.R;
+import com.example.atomica.WebViewController;
+import com.example.atomica.databinding.FragmentWebBinding;
 
 public class WebFragment extends Fragment {
 
@@ -17,11 +20,53 @@ public class WebFragment extends Fragment {
         // Required empty public constructor
     }
 
+    WebViewController controller;
+    FragmentWebBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_web, container, false);
-        return view;
+        binding = FragmentWebBinding.inflate(inflater,container,false);
+        controller = new WebViewController();
+        binding.web.loadUrl("https://graphoverflow.com/graphs/3d-periodic-table.html");
+        binding.web.setWebViewClient(controller);
+        setupWebView();
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if(binding.web.canGoBack())
+                {
+                    binding.web.goBack();
+                }
+                else{
+                    requireActivity().getSupportFragmentManager().popBackStack();
+                }
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
+        return binding.getRoot();
     }
+    private void setupWebView() {
+        // Enable JavaScript
+        binding.web.getSettings().setJavaScriptEnabled(true);
+        // Enable DOM storage (important for some dynamic websites)
+        binding.web.getSettings().setDomStorageEnabled(true);
+        // Enable viewport scaling and adjust page width
+        binding.web.getSettings().setUseWideViewPort(true);
+        binding.web.getSettings().setLoadWithOverviewMode(true);
+        // Enable zoom support
+        binding.web.getSettings().setSupportZoom(true);
+        binding.web.getSettings().setBuiltInZoomControls(true);
+        binding.web.getSettings().setDisplayZoomControls(false);
+        // Enable scrolling
+        binding.web.setHorizontalScrollBarEnabled(true);
+        binding.web.setVerticalScrollBarEnabled(true);
+
+    }
+
+
+
+
+
 }
